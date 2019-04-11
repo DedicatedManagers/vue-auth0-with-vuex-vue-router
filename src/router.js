c394c073a6353b0b3a2a94689ca1b8953fb923a5
TerryCaliendo <Terry@DedicatedManagers.com>
2019-04-10 21:22:56 -0700

    Completing mutations and actions for full login functionality

--- src/router.js
+++ src/router.js
@@ -4,8 +4,10 @@ import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Contact from './views/Contact.vue'
 import Members from './views/Members.vue'
+import Auth0Callback from './views/Auth0Callback.vue'
 import Login from './views/Login.vue'
 import Store from './store'
+
 
 Vue.use(Router)
 
@@ -13,6 +15,11 @@ const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/auth0callback',
+      name: 'auth0callback',
+      component: Auth0Callback,
+    },
     {
       path: '/',
       name: 'home',
@@ -43,7 +50,24 @@ const router = new Router({
 });
 
 router.beforeEach( (to,from,next)=>{
-  let routerAuthCheck = false;  //TODO: add actual check
+  // Allow finishing callback url for logging in
+  if(to.matched.some(record=>record.path == "/auth0callback")){
+    console.log("router.beforeEach found /auth0callback url");
+    Store.dispatch('auth0HandleAuthentication');
+    next(false);
+  }
+
+  // check if user is logged in (start assuming the user is not logged in = false)
+  let routerAuthCheck = false;  
+  // Verify all the proper access variables are present for proper authorization
+  if( localStorage.getItem('access_token') && localStorage.getItem('id_token') && localStorage.getItem('expires_at') ){
+    console.log('found local storage tokens');
+    // Check whether the current time is past the Access Token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    // set localAuthTokenCheck true if unexpired / false if expired
+    routerAuthCheck = new Date().getTime() < expiresAt;  
+  }
+
   if (routerAuthCheck)
   {
 //    Store.state.userIsAuthorized = true;  // don't do this!
--- src/store.js
+++ src/store.js
@@ -1,18 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth0 from 'auth0-js';
+import router from './router'
 
-Vue.use(Vuex)
+Vue.use(Vuex) 
 
 export default new Vuex.Store({
   state: {
     userIsAuthorized:false,
+    auth0: new auth0.WebAuth({
+      domain: "vuejs-auth0.auth0.com", 
+      clientID: "ArhqWZ5uzU_pKTHmcBMId9DlZcoSgYQr",
+      redirectUri: 'http://localhost:8080/auth0callback',  
+      responseType: "token id_token",
+      scope: "openid profile email",
+    }),
   },
   mutations: {
     setUserIsAuthenticated(state, replacement){
       state.userIsAuthorized = replacement;
-    }
+    },
+
   },
   actions: {
+    auth0Login(context){
+      context.state.auth0.authorize()
+    },
+    auth0HandleAuthentication (context) {
+      context.state.auth0.parseHash((err, authResult) => {
+        if (authResult && authResult.accessToken && authResult.idToken) {
+          let expiresAt = JSON.stringify(
+            authResult.expiresIn * 1000 + new Date().getTime()
+          )
+          // save the tokens locally
+          localStorage.setItem('access_token', authResult.accessToken);
+          localStorage.setItem('id_token', authResult.idToken);
+          localStorage.setItem('expires_at', expiresAt);  
+
+          router.replace('dashboard');
+        } 
+        else if (err) {
+          alert('login failed. Error #KJN838');
+          router.replace('/login');
+          console.log(err);
+        }
+      })
+    },
 
   }
 })
